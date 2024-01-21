041041b0ef3dcb399f74e1cb042d2669f076dcbd
FloyzenCode <vasyplay323@gmail.com>
2024-01-21 21:27:02 +0400

    add lab1

---
+++ lab1/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("Майоров\n");
+  printf("Василий\n");
+  printf("Васильевич\n");
+  printf("\n");
+  printf("Группа: ИСдо-13\n");
+  printf("Дата: 21.01.2024\n");
+  printf("Предмет: Основы программирования\n");
+  printf("Лабораторная работа №1\n");
+  return 0;
+}
---
+++ lab1/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("Лабораторная работа №1\n");
+  printf("Предмет: Основы программирования\n");
+  printf("Дата: 21.01.2024\n");
+  printf("Группа: ИСдо-13\n");
+  printf("\n");
+  printf("Васильевич\n");
+  printf("Василий\n");
+  printf("Майоров\n");
+  return 0;
+}
